b5c898fe5d6c0d85a575f897a4d5f9e67704ac84
unknown <justin_bunker@hotmail.com>
2019-05-13 17:22:57 +0100

    Refactor synthetic_data_functions.py

--- src/synthetic_data_functions.py
+++ src/synthetic_data_functions.py
@@ -8,6 +8,10 @@ def eps():
     return np.random.normal(0, NOISE_VAR)
 
 
+def add_noise(f):
+    return lambda x: f(x) + eps()
+
+
 def y1(x):
     return -sin(10 * pi * (x + 1)) / (2 * x + 1) - x ** 4
 
@@ -20,13 +24,6 @@ def y3(x):
     return y2(x) * (y1(x) ** 2) + 3 * x
 
 
-def y1_noisy(x):
-    return -sin(10 * pi * (x + 1)) / (2 * x + 1) - x ** 4 + eps()
-
-
-def y2_noisy(x):
-    return cos(y1_noisy(x)) ** 2 + sin(3 * x) + eps()
-
-
-def y3_noisy(x):
-    return y2_noisy(x) * (y1(x) ** 2) + 3 * x + eps()
+y1_noisy = add_noise(y1)
+y2_noisy = add_noise(y2)
+y3_noisy = add_noise(y3)
