cba925f0daa9c2f5522eb30b12551d9866ebb228
unknown <justin_bunker@hotmail.com>
2019-06-14 18:10:39 +0100

    Update practical function experiment for clearer results

--- exp/function_relationships/main_practical.py
+++ exp/function_relationships/main_practical.py
@@ -1,21 +1,37 @@
+import GPy
 import numpy as np
 from experiment_runner import ExperimentRunner
-from kernels import get_non_linear_input_dependent_kernel
 from utils import get_visible_index_bool
 from data_loader import get_processed_data
 
 np.random.seed(17)
 
-NUM_RESTARTS = 10
+
+def custom_kernel(original_X, current_X):
+    X_dim = original_X.shape[1]
+    Y_dim = current_X.shape[1] - X_dim
+    k1 = GPy.kern.RBF(input_dim=X_dim, active_dims=list(range(X_dim)))
+    k_linear = GPy.kern.Linear(input_dim=X_dim, active_dims=list(range(X_dim)))
+    if Y_dim > 0:
+        k2 = GPy.kern.RatQuad(input_dim=X_dim, active_dims=list(range(X_dim)))
+        return k1 + k_linear + k2
+    return k1 + k_linear
+
+
+NUM_RESTARTS = 35
 NUM_INDUCING = 100
-PERCENT_VISIBLE = 5
-KERNEL_FUNCTION = get_non_linear_input_dependent_kernel
+PERCENT_VISIBLE = 30
+KERNEL_FUNCTION = custom_kernel
 DATA_SRC = ['GOOG', 'NDXT']
 
 X_true, Y_true, labels = get_processed_data(DATA_SRC)
 
 # Construct observations
 visible_index_bool = get_visible_index_bool(X_true.shape[0], PERCENT_VISIBLE)
+
+for i in range(500, 801):
+    visible_index_bool[i] = False
+
 X_obs = X_true[visible_index_bool]
 Y_obs = Y_true[visible_index_bool]
 
