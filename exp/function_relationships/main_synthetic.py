7f5731c38e292f7acfd510152048e2025661f40d
unknown <justin_bunker@hotmail.com>
2019-06-27 21:49:07 +0100

    Rename n to n_obs

--- exp/function_relationships/main_synthetic.py
+++ exp/function_relationships/main_synthetic.py
@@ -10,8 +10,8 @@ NUM_RESTARTS = 35
 KERNEL_FUNCTION = get_non_linear_input_dependent_kernel
 
 # Construct synthetic observations
-n = 50
-X_obs = np.linspace(0, 1, n).reshape((n, 1))
+n_obs = 50
+X_obs = np.linspace(0, 1, n_obs).reshape((n_obs, 1))
 Y_obs = map_and_stack_outputs(noisy_synthetic_functions, X_obs)
 
 # Construct true outputs
