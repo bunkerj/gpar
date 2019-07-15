4cead1bc75b23fe9b40920e609f0451b515775e0
unknown <justin_bunker@hotmail.com>
2019-07-15 10:45:12 +0100

    Refactor mse implementation

--- src/evaluation.py
+++ src/evaluation.py
@@ -16,11 +16,6 @@ def smse(series1, series2):
     return float(error_acc / n)
 
 
-def mse(series1, series2):
-    validate_lengths(series1, series2)
-    n = len(series1)
-    error_acc = 0
-    for idx in range(n):
-        e = series1[idx] - series2[idx]
-        error_acc += (e ** 2)
-    return float(error_acc / n)
+def mse(x, y):
+    validate_lengths(x, y)
+    return np.mean((x - y) ** 2)
