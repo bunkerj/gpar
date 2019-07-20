251b45487f186882382603840f33e38e52ddd861
unknown <justin_bunker@hotmail.com>
2019-07-20 19:12:02 +0100

    Update freeze_thaw main script

--- exp/freeze_thaw/main.py
+++ exp/freeze_thaw/main.py
@@ -2,11 +2,13 @@ from exp.freeze_thaw.random_search import RandomSearch
 from exp.freeze_thaw.freeze_thaw import FreezeThaw
 from matplotlib import pyplot as plt
 
-N_SAMPLES = 20
-N_EPOCHS = 10
+N_SAMPLES = 3
+N_EPOCHS = 2
 bounds_list = [[1, 10], [5, 500]]
+B_OLD = 10
+B_NEW = 3
 
-freeze_thaw = FreezeThaw(bounds_list)
+freeze_thaw = FreezeThaw(bounds_list, B_OLD, B_NEW)
 freeze_thaw.run()
 freeze_thaw.plot()
 
