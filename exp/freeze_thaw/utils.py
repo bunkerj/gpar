6429d7b3f6b9155a378c3338dec5f89edda41a52
unknown <justin_bunker@hotmail.com>
2019-07-22 14:58:12 +0100

    Add further freeze_thaw utils implementations

--- exp/freeze_thaw/utils.py
+++ exp/freeze_thaw/utils.py
@@ -1,6 +1,17 @@
+import scipy
+import tensorflow as tf
+import numpy as np
+
+
 def hyp_to_key(hyp):
     return '_'.join(str(v) for v in hyp)
 
 
 def key_to_hyp(key):
     return tuple(int(v) for v in key.split('_'))
+
+
+def get_O(count_list):
+    arrays = [np.ones((count, 1)) for count in count_list]
+    O_raw = scipy.linalg.block_diag(*arrays)
+    return tf.constant(O_raw)
