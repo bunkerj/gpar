5d24292eb898b75d14f56f1c1a2785dfcd1ae940
unknown <justin_bunker@hotmail.com>
2019-07-22 20:55:03 +0100

    Add utils function to create a block diagonal matrix in tensorflow to allow gradients to propagate

--- exp/freeze_thaw/utils.py
+++ exp/freeze_thaw/utils.py
@@ -1,6 +1,4 @@
-import scipy
 import tensorflow as tf
-import numpy as np
 
 
 def hyp_to_key(hyp):
@@ -11,7 +9,24 @@ def key_to_hyp(key):
     return tuple(int(v) for v in key.split('_'))
 
 
-def get_O(count_list):
-    arrays = [np.ones((count, 1)) for count in count_list]
-    O_raw = scipy.linalg.block_diag(*arrays)
-    return tf.constant(O_raw)
+def concat_tensors(tensor_list, axis):
+    filtered_tensor_list = list(filter(lambda x: x is not None, tensor_list))
+    return tf.concat(filtered_tensor_list, axis)
+
+
+def get_block_diag_matrix(matrix_list):
+    col_index = 0
+    current_matrix = None
+    total_columns = sum(int(tf.shape(mat)[1]) for mat in matrix_list)
+    for mat in matrix_list:
+        n_rows, n_cols = tf.shape(mat)
+        front_padding = None if int(col_index) == 0 else tf.zeros((n_rows, col_index))
+        back_padding = None if int(col_index + n_cols) == total_columns else tf.zeros(
+            (n_rows, total_columns - (col_index + n_cols)))
+        sub_matrix = concat_tensors([front_padding, mat, back_padding], 1)
+        col_index += n_cols
+        if current_matrix is None:
+            current_matrix = sub_matrix
+        else:
+            current_matrix = tf.concat([current_matrix, sub_matrix], axis=0)
+    return current_matrix
