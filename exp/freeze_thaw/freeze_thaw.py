ae0a67f48cac462584367fef49363647c3d7f624
unknown <justin_bunker@hotmail.com>
2019-07-22 14:57:45 +0100

    Add skeleton implementation for computing the log likelihood in the FreezeThaw class

--- exp/freeze_thaw/freeze_thaw.py
+++ exp/freeze_thaw/freeze_thaw.py
@@ -1,7 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from src_utils import get_bounded_samples
+from optimizer import Optimizer
+from exp.freeze_thaw.utils import get_O
 from exp.freeze_thaw.aggregators.model_aggregator import ModelAggregator
+from exp.freeze_thaw.aggregators.param_aggregator import ParamAggregator
 
 
 class FreezeThaw:
@@ -12,8 +15,9 @@ class FreezeThaw:
         self.b_new = b_new
         self.max_epoch = max_epoch
         self.init_epochs = init_epochs
-        self.model_aggregator = None
-        self.param_aggregator = None
+        self.ma = None
+        self.pa = None
+        self.optimizer = Optimizer(learning_rate=0.1, n_epochs=10)
 
     def run(self):
         self._train_initial_models()
@@ -25,7 +29,7 @@ class FreezeThaw:
 
     def plot(self):
         """Plot all losses."""
-        losses = self.model_aggregator.get_all_losses()
+        losses = self.ma.get_all_losses()
         for key in losses:
             loss = losses[key]
             x = self._get_suitable_indices(loss)
@@ -33,15 +37,15 @@ class FreezeThaw:
 
     def _train_initial_models(self):
         hyp_list = get_bounded_samples(self.hyp_bounds_list, self.b_old)
-        self.model_aggregator = ModelAggregator(hyp_list)
-        self.model_aggregator.train_all_models(self.init_epochs)
+        self.ma = ModelAggregator(hyp_list)
+        self.ma.train_all_models(self.init_epochs)
 
     def _construct_key_basket(self):
         """
         Use EI to compute this basket.
         Return: list of model keys.
         """
-        return self.model_aggregator.get_all_keys()
+        return self.ma.get_all_keys()
 
     def _select_model_to_train(self, key_basket):
         """
@@ -54,10 +58,42 @@ class FreezeThaw:
     def _train_model_from_basket(self, basket):
         model_key = self._select_model_to_train(basket)
         print('Training model {}'.format(model_key))
-        self.model_aggregator.train_model_given_key(model_key, 1)
+        self.ma.train_model_given_key(model_key, 1)
+
+    def _get_log_likelihood(self, y, O, m):
+        """
+        y: (y_1, y_2, ... , y_n)^T
+        m: (m_1, m_2, ... , m_n)^T
+        K_t: blockdiag(K_t1, K_t2, ... , K_tn)
+        K_x: standard cov matrix (using RBF)
+        O: blockdiag(1_1, 1_2, ... , 1_n)
+        """
+        K_t = self.pa.get_K_t()
+        K_x = self.pa.get_K_x()
+        return 3 + 2  # TODO: Construct loss expression.
+
+    def _print_loss(self, args):
+        print('Loss: {}'.format(self._get_log_likelihood(*args)))
 
     def _update_hyperparameters(self):
         print('Updating the hyperparameters...')
+        if self.pa is None:
+            self.pa = ParamAggregator(self.ma.get_curve_count())
+
+        y = self.ma.get_stacked_losses()
+        O = get_O(self.ma.get_loss_count_per_curve())
+        m = self.pa.get_global_means()
+        global_kernel_param_list = self.pa.get_global_kernel_param_list()
+        local_kernel_param_list = self.pa.get_local_kernel_param_list()
+
+        params = [m] + global_kernel_param_list + local_kernel_param_list
+        args = (y, O, m)
+
+        print('Loss Before: {}'.format(self._get_log_likelihood(*args)))
+        self.optimizer.minimize_loss(self._get_log_likelihood, params, args)
+        print('Loss Before: {}'.format(self._get_log_likelihood(*args)))
+
+        a = 10  # TODO: Check if changes were performed.
 
     def _get_suitable_indices(self, arr):
         return np.arange(1, arr.shape[0] + 1).reshape((-1, 1))
