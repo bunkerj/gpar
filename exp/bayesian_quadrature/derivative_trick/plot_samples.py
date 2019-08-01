5aa7708c1c19b5a9a38e5bdef832de38cf273026
unknown <justin_bunker@hotmail.com>
2019-08-01 20:45:29 +0100

    Update plot_samples script

--- exp/bayesian_quadrature/derivative_trick/plot_samples.py
+++ exp/bayesian_quadrature/derivative_trick/plot_samples.py
@@ -1,32 +1,32 @@
 import numpy as np
-from exp.bayesian_quadrature.derivative_trick.kernels import RBF_dd
 from matplotlib import pyplot as plt
+from src.kernels import ExponentialDecay
+from exp.bayesian_quadrature.derivative_trick.kernels import RBF_dd
 from gpflow.kernels import Matern12, Matern32, Matern52, \
-    RBF, Constant, Linear, Cosine
+    RBF, Constant, Linear, Cosine, RationalQuadratic
 
 
-def plot_kernel_sample(k, ax, xmin=0, xmax=6):
+def plot_kernel_sample(k, xmin=0, xmax=6):
     xx = np.linspace(xmin, xmax, 100)[:, None]
     K = k.compute_K_symm(xx)
-    ax.plot(xx, np.random.multivariate_normal(np.zeros(100), K, 3).T)
-    ax.set_title(k.__class__.__name__)
+    plt.plot(xx, np.random.multivariate_normal(np.zeros(100), K, N_SAMPLES).T)
+    plt.title(k.__class__.__name__)
 
 
-NROWS = 2
-NCOLS = 4
+N_SAMPLES = 5
+NROWS = 1
+NCOLS = 1
 
-kernels = [Matern12, Matern32, Matern52, RBF,
-           Constant, Linear, Cosine, RBF_dd]
+kernels = [ExponentialDecay]
 
 assert len(kernels) == NROWS * NCOLS
 
-f, axes = plt.subplots(NROWS, NCOLS, figsize=(12, 6))
+plt.subplots(NROWS, NCOLS, figsize=(12, 6))
 
 for idx, kernel in enumerate(kernels):
     row = idx // NCOLS
     col = idx % NCOLS
-    plot_kernel_sample(kernel(1), axes[row, col])
-
-axes[0, 0].set_ylim(-3, 3)
+    plt.subplot(NROWS, NCOLS, idx + 1)
+    plot_kernel_sample(kernel())
 
 plt.show()
