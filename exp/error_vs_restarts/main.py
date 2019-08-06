a5c057137a868e19c694208afce9d3f5e11d58ab
unknown <justin_bunker@hotmail.com>
2019-08-06 14:41:41 +0100

    Update main in error_vs_restarts to test multiple kernel configs

--- exp/error_vs_restarts/main.py
+++ exp/error_vs_restarts/main.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 import numpy as np
-from kernels import get_full_rbf_kernel
+from kernels import get_full_rbf_kernel, get_non_linear_input_dependent_kernel
 from src_utils import map_and_stack_outputs
 from utils import get_total_smse_values_and_ordering_index
 from constants import OUTPUTS_PATH, NUM_RESTARTS_VALUES_PATH
@@ -10,18 +10,23 @@ from synthetic_functions import \
     medium_complexity_functions, noisy_medium_complexity_functions, \
     high_complexity_functions, noisy_high_complexity_functions
 
-KERNEL_FUNCTION = get_full_rbf_kernel
 NUM_AVG_SAMPLES = 20
 
 AGGREGATE_FUNCTIONS_DICT = {
-    'low': (low_complexity_functions, noisy_low_complexity_functions),
-    'medium': (medium_complexity_functions, noisy_medium_complexity_functions),
-    'high': (high_complexity_functions, noisy_high_complexity_functions)
+    'low_rbf': (low_complexity_functions, noisy_low_complexity_functions, get_full_rbf_kernel),
+    'medium_rbf': (medium_complexity_functions, noisy_medium_complexity_functions, get_full_rbf_kernel),
+    'high_rbf': (high_complexity_functions, noisy_high_complexity_functions, get_full_rbf_kernel),
+    'low_custom_kernel': (
+        low_complexity_functions, noisy_low_complexity_functions, get_non_linear_input_dependent_kernel),
+    'medium_custom_kernel': (
+        medium_complexity_functions, noisy_medium_complexity_functions, get_non_linear_input_dependent_kernel),
+    'high_custom_kernel': (
+        high_complexity_functions, noisy_high_complexity_functions, get_non_linear_input_dependent_kernel)
 }
 
-for key in AGGREGATE_FUNCTIONS_DICT.keys():
-    true_functions, noisy_functions = AGGREGATE_FUNCTIONS_DICT[key]
-    filename = '{}{}_complexity.pickle'.format(OUTPUTS_PATH, key)
+for i, key in enumerate(AGGREGATE_FUNCTIONS_DICT.keys()):
+    true_functions, noisy_functions, kernel = AGGREGATE_FUNCTIONS_DICT[key]
+    filename = '{}{}_{}.pickle'.format(OUTPUTS_PATH, i, key)
 
     # Construct synthetic observations
     n = 50
@@ -33,10 +38,10 @@ for key in AGGREGATE_FUNCTIONS_DICT.keys():
     X_new = np.linspace(0, 5, n_new).reshape((n_new, 1))
     Y_true = map_and_stack_outputs(true_functions, X_new)
 
-    num_restarts_values = np.array([1, 5, 10, 20, 40, 50]).reshape((-1, 1))
+    num_restarts_values = np.array([1, 5, 10, 15, 20, 25, 30]).reshape((-1, 1))
 
     total_mse_values = get_total_smse_values_and_ordering_index(
-        X_obs, Y_obs, X_new, Y_true, KERNEL_FUNCTION,
+        X_obs, Y_obs, X_new, Y_true, kernel,
         num_restarts_values, NUM_AVG_SAMPLES)
 
     if not os.path.exists(NUM_RESTARTS_VALUES_PATH):
