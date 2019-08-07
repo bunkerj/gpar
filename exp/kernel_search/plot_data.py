983ceedce0368f83282db828844cf31d58c9131e
unknown <justin_bunker@hotmail.com>
2019-08-07 23:40:03 +0100

    Update plotting script for kernel_search

--- exp/kernel_search/plot_data.py
+++ exp/kernel_search/plot_data.py
@@ -1,7 +1,26 @@
 import pickle
 from exp.kernel_search.constants import DATA_PATH
+from matplotlib import pyplot as plt
+
+BOUND = 10
 
 with open(DATA_PATH, 'rb') as file:
     likelihood_data = pickle.load(file)
 
-print(likelihood_data)
+n = len(likelihood_data)
+sorted_data = {k: v for k, v in
+               sorted(likelihood_data.items(), key=lambda x: x[1])}
+
+for idx, key in enumerate(list(sorted_data)):
+    if BOUND <= idx < n - BOUND:
+        del sorted_data[key]
+
+print(sorted_data)
+
+plt.barh(list(sorted_data.keys()),
+         list(sorted_data.values()),
+         align='center')
+plt.title('Kernel Log-likelihoods for Kernel Search')
+plt.xlabel('Log-likelihood')
+plt.grid(axis='x')
+plt.show()
