fe388f0e2c037fcaa0442567bbf9a8e2ccfc6f6b
unknown <justin_bunker@hotmail.com>
2019-08-07 22:39:55 +0100

    Refactor kernel_search to save likelihood data and improve printing

--- exp/kernel_search/main.py
+++ exp/kernel_search/main.py
@@ -1,3 +1,4 @@
+import pickle
 from exp.kernel_search.data_source import generate_data
 from exp.kernel_search.utils import generate_kernels, get_total_log_likelihood, print_kernel
 from exp.function_relationships.experiment_runner import ExperimentRunner
@@ -5,9 +6,9 @@ from kernels import get_non_linear_input_dependent_kernel
 from matplotlib import pyplot as plt
 from src.regression.gpar_regression import GPARRegression
 
-DEPTH = 2
+DEPTH = 1
 N_SAMPLES = 1
-N_RESTARTS = 5
+N_RESTARTS = 1
 
 X_obs, Y_obs, X_new, Y_true = generate_data()
 
@@ -15,6 +16,8 @@ max_log_likelihood = None
 best_kernel = None
 best_kernel_string = None
 current_kernels = []
+
+likelihood_data = {}
 
 for i in range(DEPTH):
     current_kernels = generate_kernels(current_kernels)
@@ -24,12 +27,14 @@ for i in range(DEPTH):
         kernel.print()
         raw_kernel = kernel.get_raw_kernel()
         total_log_likelihood = get_total_log_likelihood(X_obs, Y_obs, raw_kernel, N_RESTARTS, N_SAMPLES)
+        likelihood_data[kernel.get_kernel_string()] = total_log_likelihood
         if (max_log_likelihood is None) or (total_log_likelihood > max_log_likelihood):
             max_log_likelihood = total_log_likelihood
             best_kernel = raw_kernel
             best_kernel_string = kernel.get_kernel_string()
 
-print(best_kernel_string)
+# --------------------------- Compute Error and Display Fit --------------------------- #
+
 
 exp_kernel_search = ExperimentRunner(X_obs, Y_obs, X_new, Y_true,
                                      best_kernel, N_RESTARTS,
@@ -41,10 +46,15 @@ exp_regular = ExperimentRunner(X_obs, Y_obs, X_new, Y_true,
                                N_RESTARTS, figure_start=10)
 exp_regular.run()
 
-print('Search log-likelihood: {}'.format(max_log_likelihood))
+# --------------------------- Print and Save Relevant Data --------------------------- #
+
+with open('results/likelihood_data', 'wb') as file:
+    pickle.dump(likelihood_data, file)
 
 m = GPARRegression(X_obs, Y_obs, get_non_linear_input_dependent_kernel, num_restarts=N_RESTARTS)
 
-print('Standard log-likelihood: {}'.format(m.get_total_log_likelihood()))
+print('-----------------------------------------')
+print('[{}] log-likelihood: {}'.format(best_kernel_string, max_log_likelihood))
+print('Baseline log-likelihood: {}'.format(m.get_total_log_likelihood()))
 
 plt.show()
