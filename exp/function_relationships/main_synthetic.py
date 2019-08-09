845f29f57641915426b8bda64197a84003d5387a
unknown <justin_bunker@hotmail.com>
2019-08-09 16:08:44 +0100

    Update function_relationships main

--- exp/function_relationships/main_synthetic.py
+++ exp/function_relationships/main_synthetic.py
@@ -2,23 +2,24 @@ import numpy as np
 from matplotlib import pyplot as plt
 from src.src_utils import map_and_stack_outputs
 from exp.function_relationships.experiment_runner import ExperimentRunner
-from src.kernels import get_non_linear_input_dependent_kernel
-from src.synthetic_functions import synthetic_functions, noisy_synthetic_functions
+from src.kernels import get_non_linear_input_dependent_kernel, get_full_rbf_kernel
+from src.synthetic_functions import gaussian_functions, bessel_functions
 
-np.random.seed(17)
-
-NUM_RESTARTS = 35
-KERNEL_FUNCTION = get_non_linear_input_dependent_kernel
+NUM_RESTARTS = 1000
+KERNEL_FUNCTION = get_full_rbf_kernel
+START = -8
+END = 0.5
+FUNCTIONS = gaussian_functions
+N_OBS = 5
 
 # Construct synthetic observations
-n_obs = 50
-X_obs = np.linspace(0, 1, n_obs).reshape((n_obs, 1))
-Y_obs = map_and_stack_outputs(noisy_synthetic_functions, X_obs)
+X_obs = np.linspace(START, END, N_OBS).reshape((N_OBS, 1))
+Y_obs = map_and_stack_outputs(FUNCTIONS, X_obs)
 
 # Construct true outputs
 n_new = 1000
-X_new = np.linspace(0, 1, n_new).reshape((n_new, 1))
-Y_true = map_and_stack_outputs(synthetic_functions, X_new)
+X_new = np.linspace(START, END, n_new).reshape((n_new, 1))
+Y_true = map_and_stack_outputs(FUNCTIONS, X_new)
 
 # Run experiment
 exp = ExperimentRunner(X_obs, Y_obs, X_new, Y_true, KERNEL_FUNCTION, NUM_RESTARTS)
